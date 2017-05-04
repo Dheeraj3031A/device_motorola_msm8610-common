3d54bf3e531a7662525a2a7ad95d72058c9e7f54
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    msm8610-common: Create media_profiles_V1_0.xml
    * Also move media configs to vendor
    
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    From: Pawin Vongmasa <pawin@google.com>
    Date: Wed May 03 22:42:57 2017 -0700
    
        Remove the property media.settings.xml.
    
        This CL also removes media_profiles.xml as it should be replaced by
        media_profiles_V1_0.xml.
    
        Bug: 36430270
        Merged-In: I4299abfde44ced36560736520d0be592894d00a7
        Change-Id: I4299abfde44ced36560736520d0be592894d00a7
    
    Merged-In: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Signed-off-by: Chippa-a <vusal1372@gmail.com>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: RenanQueiroz <queirozrrq@gmail.com>

--- msm8610.mk
+++ msm8610.mk
@@ -78,9 +78,9 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/vendor/etc/media_codecs_google_video_le.xml
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -165,8 +165,8 @@ PRODUCT_PACKAGES +=  \
 
 # Media Config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/vendor/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml
 
 # Motorola
 PRODUCT_PACKAGES += \
