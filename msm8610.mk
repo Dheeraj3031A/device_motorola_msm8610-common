d93eab2ce1ef09278c208eaff11d4486713bba00
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    msm8610-common: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- msm8610.mk
+++ msm8610.mk
@@ -238,7 +238,12 @@ PRODUCT_PACKAGES += \
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
-    
+
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
