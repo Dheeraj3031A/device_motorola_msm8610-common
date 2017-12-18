57c217b49a74a363854ceb518dad6a74ce6b2a77
Bruno Martins <bgcngm@gmail.com>
2017-12-18 11:20:17 +0000

    msm8610-common: Use XML audio policy configuration file
     * Switch to audio_policy_configuration.xml instead of using
       the legacy audio_policy.conf.
    
     * Removed multi-channel profile as it is redundant with respect
       to direct output profiles [1].
    
    [1] https://source.codeaurora.org/quic/la/platform/hardware/qcom/audio/commit/?h=LA.UM.6.5.r1-04300-8x96.0&id=ac3415870d63b18694e49d982e89ccb10dbcb2c3
    
    Change-Id: Ia93e30428ff9e27ae409e685a84fa5a8e406bca0

--- msm8610.mk
+++ msm8610.mk
@@ -119,8 +119,15 @@ PRODUCT_PACKAGES += \
 
 # Audio Configuration
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(COMMON_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
+    $(COMMON_PATH)/configs/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(COMMON_PATH)/configs/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
