395604689e32d375b768547ae6a46bd81acd4dbc
Vachounet <vachounet@live.fr>
2018-01-11 13:57:17 +0100

    msm8610-common: Build configstore service
    Change-Id: Id3250d6da32709b4aac6be6e82f3dc4ddef1230b

--- msm8610.mk
+++ msm8610.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.configstore@1.1-service \
     android.hardware.memtrack@1.0-impl \
     gralloc.msm8610 \
     copybit.msm8610 \
