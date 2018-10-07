a0b3d497346a69bb42bf87757237f1d78551e17e
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    msm8610-common: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
     * Furthermore, with System As Root / is a read-only filesystem
       so we can't create our mountpoints from init script anymore
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 vmalloc=400M loop.max_part=7 androidboot.selinux=permissive
+BOARD_ROOT_EXTRA_FOLDERS := firmware fsg persist
 LZMA_RAMDISK_TARGETS := recovery
 
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
