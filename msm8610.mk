654a6f06affa7eeff3507a632adebe05afe8e514
RenanQueiroz <queirozrrq@gmail.com>
2019-01-14 23:21:06 -0500

    msm8610-common: Don't build soundtrigger hal
    * Not used by these devices

--- msm8610.mk
+++ msm8610.mk
@@ -112,8 +112,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.broadcastradio@1.0-impl
 
 # Audio Configuration
 PRODUCT_COPY_FILES += \
