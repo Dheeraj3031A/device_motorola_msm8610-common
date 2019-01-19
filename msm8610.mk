499be8ba6642a1c432199279d8d7064d9b066f20
RenanQueiroz <queirozrrq@gmail.com>
2019-01-19 15:53:39 -0500

    msm8610-common: Preopt some Lineage default apps

--- msm8610.mk
+++ msm8610.mk
@@ -261,7 +261,9 @@ PRODUCT_PACKAGES += \
 # Preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
-    SystemUI
+    Snap \
+    SystemUI \
+    TrebuchetQuickStep
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
