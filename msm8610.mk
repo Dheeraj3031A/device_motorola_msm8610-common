8908e8a055606a71d78088dbf9c0f173f47d0978
RenanQueiroz <queirozrrq@gmail.com>
2019-11-03 23:51:45 -0500

    msm8610-common: Add triple buffering back
     * Fixes tearing
     * Also change SF_START_GRAPHICS_ALLOCATOR_SERVICE into a prop, as configstore build flags have been deprecated in 10
    
    This partially reverts commit cc4e308a68fe57ccba2bd6032a1380bd1dd3c0ac.

--- msm8610.mk
+++ msm8610.mk
@@ -244,6 +244,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
+# Display
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.start_graphics_allocator_service=true
+
 # Ebtables
 PRODUCT_PACKAGES += \
     ebtables \
