5d9eff9f9bd026e4e1bca59979d031638da34796
Dheeraj R Shrivatsa <Dheeraj3031A@users.noreply.github.com>
2025-09-10 19:50:06 +0530

    Revert "Reapply "msm8610-common: Enforce RRO on framework-res""
    This reverts commit bd362abc966e53b73ca7d1cc7016d2f22658a28e.

--- msm8610.mk
+++ msm8610.mk
@@ -85,9 +85,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # WiFi configuration
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
