45358d08144920ce3ffc22b1db89061eee66e825
Dheeraj R Shrivatsa <Dheeraj3031A@users.noreply.github.com>
2025-09-10 13:35:44 +0530

    Revert "msm8610-common: Build LegacyMM livedisplay HAL"
    This reverts commit b691a31e67d0b303151dc31dd32688474edae1eb.

--- msm8610.mk
+++ msm8610.mk
@@ -242,10 +242,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8610
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
-
 # GNSS HAL
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-imp
